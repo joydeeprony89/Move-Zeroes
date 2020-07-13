cad7e644fb14b21daca5c6a14d7dd5fcff018928
joydeeprony89 <joydeeprony89@gmail.com>
2020-07-13 09:09:58 +0200

    Add project files.

---
+++ Move Zeroes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Move_Zeroes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 0, 1, 0, 12 };
+            MoveZeroes(nums);
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void MoveZeroes(int[] nums)
+        {
+            for (int lastNonZeroIndex = 0, cur = 0; cur < nums.Length; cur++)
+            {
+                if(nums[cur] != 0)
+                {
+                    int temp = nums[cur];
+                    int prev = nums[lastNonZeroIndex];
+                    nums[lastNonZeroIndex++] = temp;
+                    nums[cur] = prev;
+                }
+            }
+        }
+    }
+}
